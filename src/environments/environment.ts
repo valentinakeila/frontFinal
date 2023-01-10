cd66ac108f44183a85a1b99d64c605369dacde7b
Valen <valentinakeila12@gmail.com>
2023-01-10 20:44:52 -0300

    front

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   URL: 'https://backfinal-production-29e7.up.railway.app/'
 };
 
